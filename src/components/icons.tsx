80eb6a6f8ba0ea75864696b8afb26444edfefe16
SOURAV PAUL <paul23sourav@gmail.com>
2026-02-24 05:33:17 +0000

    In header, logo appears bit on top.. Increase the font size a bit and al

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,16 +4,17 @@ export const PaulLogo = ({ className, ...props }: React.SVGProps<SVGSVGElement>)
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 100 20"
-    className={cn("h-5 w-auto text-primary", className)}
+    className={cn("h-6 w-auto text-primary", className)}
     {...props}
     fill="currentColor"
   >
     <title>PAUL</title>
     <text
       x="0"
-      y="15"
+      y="50%"
+      dominantBaseline="middle"
       fontFamily="Inter, sans-serif"
-      fontSize="18"
+      fontSize="20"
       fontWeight="600"
       letterSpacing="0.2em"
     >
